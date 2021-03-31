24e17a46758313133e362bce8060529cfefabb47
Romi <loromi94@gmail.com>
2021-03-31 17:28:07 +0200

    feat: defined function for creating a grid of boxes

--- designs.js
+++ designs.js
@@ -1,1 +1,10 @@
 const table = document.getElementById('pixelCanvas');
+
+function makeGrid(gridWidth, gridHeight) {
+    for (let i=0; i<gridHeight; ++i) {
+        let tRow = table.insertRow(-1);
+        for (let j=0; j<gridWidth; ++j) {
+            let tCol = tRow.insertCell(-1);
+        };
+    }; 
+}
