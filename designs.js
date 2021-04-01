03be627d8992118ca08bc65319b136da0a665339
Romi <loromi94@gmail.com>
2021-04-01 12:28:17 +0200

    docs: added descriptive comments to improve readability

--- designs.js
+++ designs.js
@@ -2,8 +2,14 @@ const table = document.getElementById('pixelCanvas');
 
 const gridHeight = document.getElementById('inputHeight');
 
-const gridWidth = document.getElementById('inputWidth'); 
+const gridWidth = document.getElementById('inputWidth');
 
+/**
+ * @description fetch the input after user submits the form
+ * @description prevents reloading page when input is submitted
+ * @description runs the 'makeGrid()' function and passes the users input as arguments
+ * @param {event} createGrid - event that fires by user submission of the form
+ */ 
 const submit = document.querySelector('#sizePicker').addEventListener('submit', function (createGrid) {
     createGrid.preventDefault();
     let height = gridHeight.value;
@@ -11,6 +17,11 @@ const submit = document.querySelector('#sizePicker').addEventListener('submit', 
     makeGrid(width, height); 
 });
 
+/**
+ * @description create a HTML table that resembles a grid of squares 
+ * @param {*} width - number of columns per row = number of horizontal squares
+ * @param {*} height - number of rows = number of vertical squares
+ */
 function makeGrid(width, height) {
     table.innerHTML = '';
     for (let i=0; i<height; ++i) {
@@ -21,6 +32,10 @@ function makeGrid(width, height) {
     }; 
 }
 
+/**
+ * @description color a particular table cell when it get's clicked
+ * @param {event} cell - event that selects table cell by 'mousedown'
+*/
 table.addEventListener('mousedown', function (cell) {
     let color = document.getElementById('colorPicker').value;
     if (cell.target.nodeName === 'TD') {
